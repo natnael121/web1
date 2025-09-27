be1478a03eefa1b2193a45751be037d42bf4b189
new7pro <new7pro@basexindustries.com>
2025-09-28 00:06:43 +0300

    Fix AdminPanel default export error

---
+++ src/components/AdminPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPanel: React.FC = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Admin Panel</h1>
+      <p className="text-gray-600">Admin panel content will be implemented here.</p>
+    </div>
+  );
+};
+
+export default AdminPanel;
